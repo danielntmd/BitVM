8aec00098498a9c4c87b42dcb4deb0b92044d5d4
danielntmd <daniel.kim@nethermind.io>
2025-03-28 17:08:00 -0400

    clean: reorganize files

---
+++ fuzz/fuzz_targets/bigint/lib.rs
@@ -0,0 +1,43 @@
+pub mod macros; 
+
+use bitvm::bigint::{BigIntImpl, U254, U256, U64};
+
+pub type U384 = BigIntImpl<384, 29>;
+
+#[derive(Debug)]
+pub enum BigIntType {
+    U64(U64),
+    U254(U254),
+    U256(U256),
+    U384(U384), // We use 256bit (BN254), but test for others (e.g. BLS12-381)
+}
+
+impl BigIntType {
+    pub fn from_index(idx: u32) -> Self {
+        match idx {
+            0 => Self::U64(U64 {}),
+            1 => Self::U254(U254 {}),
+            2 => Self::U256(U256 {}),
+            3 => Self::U384(U384 {}),
+            _ => panic!("Invalid BigIntType index"),
+        }
+    }
+
+    pub fn n_bits(&self) -> u32 {
+        match self {
+            BigIntType::U64(_) => U64::N_BITS,
+            BigIntType::U254(_) => U254::N_BITS,
+            BigIntType::U256(_) => U256::N_BITS,
+            BigIntType::U384(_) => U384::N_BITS,
+        }
+    }
+
+    pub fn limb_size(&self) -> u32 {
+        match self {
+            BigIntType::U64(_) => U64::LIMB_SIZE,
+            BigIntType::U254(_) => U254::LIMB_SIZE,
+            BigIntType::U256(_) => U256::LIMB_SIZE,
+            BigIntType::U384(_) => U384::LIMB_SIZE,
+        }
+    }
+}
